26a2860ec1d7a7a38ea134689b6606b6144af8fb
Vijendra Kumar <mohit360singh@gmail.com>
2026-02-14 01:18:01 +0530

    Create rotateClockwise.py

---
+++ rotateClockwise.py
@@ -0,0 +1,16 @@
+def rotate_clockwise(arr):
+    n = len(arr)
+    if n == 0:
+        return arr
+    
+    last = arr[-1]
+    
+    for i in range(n - 1, 0, -1):
+        arr[i] = arr[i - 1]
+    
+    arr[0] = last
+    return arr
+
+
+arr = [1, 2, 3, 4, 5]
+print(rotate_clockwise(arr))
